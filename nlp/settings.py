f102553919b22256d3a1ad6e95d95b0c8668dc40
farzamTP <farzam.taghipour@gmail.com>
2020-05-14 23:11:01 +0430

    Bugs fixed.

--- nlp/settings.py
+++ nlp/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+LOGIN_URL = 'index/'
+
+LOGIN_REDIRECT_URL = '/'

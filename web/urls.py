dfc34ada169352f9d109e691ee0ef0eb4fab5771
farzamTP <farzam.taghipour@gmail.com>
2020-05-15 19:44:48 +0430

    Export method added.

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url('^api/ask$', views.classify, name='index'),
     url('^api/submit$', views.submit, name='submit'),
+    url('^api/export$', views.export_to_csv, name='export_to_csv'),
 ]
--- web/views.py
+++ web/views.py
@@ -104,9 +104,9 @@ def export_to_csv(request):
 
     sentences = Sentence.objects.all()
 
-    # for idx, sentence in enumerate(sentences):
-        # data.loc[idx] = [str(sentence.text), str(sentence.predicted_emoji), str(sentence.feedback),
-        #                  str(sentence.assigned_label), float(sentence.prob)]
+    for idx, sentence in enumerate(sentences):
+        data.loc[idx] = [str(sentence.text), str(sentence.predicted_emoji), str(sentence.feedback),
+                         str(sentence.assigned_label), float(sentence.prob)]
 
-    # data.to_csv('./data.csv')
+    data.to_csv('./data.csv')
     return JsonResponse(data={'data': 200})
